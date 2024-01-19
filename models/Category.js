d26c4ee54637f3cb2bbe0e50356736245407614e
Deboh12 <deborah.alexander12@yahoo.com>
2024-01-19 16:13:47 -0600

    defined and id column, and a category_name column in models/category.js

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,24 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+// Category model extends Sequelize's Model class
 class Category extends Model {}
 
+// Initialize the Category model with its defined columns and configuration
 Category.init(
   {
-    // define columns
+    // Define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Define a category_name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
